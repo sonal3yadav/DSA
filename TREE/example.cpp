07d2e31b48cf4af76f0d76ad6e75ad84ec294c36
sonal yadav <66108853+sonal3yadav@users.noreply.github.com>
2022-10-19 03:29:39 +0530

    Update example.cpp

--- TREE/example.cpp
+++ TREE/example.cpp
@@ -1,1 +1,52 @@
+//Link to problem:
+https://leetcode.com/problems/binary-tree-inorder-traversal/
 
+
+//Problem Statement:
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+  
+  
+//Example:
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+
+
+//SOLUTION
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*>st;
+        TreeNode* node=root;
+        vector<int>inorder;
+        while(true)
+        {
+            if(node!=NULL)
+            {
+                st.push(node);
+                node=node->left;
+            }
+            else
+            {
+                if(st.empty()==true)
+                    break;
+                node=st.top();
+                st.pop();
+                inorder.push_back(node->val);
+                node=node->right;
+            }
+        }
+        return inorder;
+    }
+};
