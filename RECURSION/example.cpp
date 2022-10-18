afd29e0187bcb2c140e6047fc18d6821abf3c6a4
sonal yadav <66108853+sonal3yadav@users.noreply.github.com>
2022-10-19 03:38:59 +0530

    Update example.cpp

--- RECURSION/example.cpp
+++ RECURSION/example.cpp
@@ -1,1 +1,52 @@
+//Link to problem:
+https://leetcode.com/problems/merge-two-sorted-lists/
 
+
+//Problem statement:
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+ 
+//Example:
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+
+//SOLUTION:
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1==NULL)return list2;
+        if(list2==NULL)return list1;
+        if(list1->val>=list2->val)
+            swap(list1,list2);
+        ListNode*res=list1;
+        while(list1!=NULL && list2!=NULL)
+        {
+            ListNode*temp=NULL;
+            while(list1!=NULL && list1->val<=list2->val)
+            {
+                temp=list1;
+                list1=list1->next;
+            }
+            temp->next=list2;
+            swap(list1,list2);
+            
+        }
+        return res;
+    }
+};
