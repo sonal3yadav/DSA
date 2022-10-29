a2b28d3d059bed8e8f40c990fa71670bb37e5a3d
Aryan <99543379+ksba2004@users.noreply.github.com>
2022-10-30 01:45:01 +0530

    Create RemoveElement.cpp
    Added a problem on Arrays of Removing Element

---
+++ Arrays/RemoveElement.cpp
@@ -0,0 +1,20 @@
+// Problem link:
+https://leetcode.com/problems/remove-element/
+
+// Code
+```class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+            int n = nums.size();
+            int j = 0;
+            for(int i = 0; i<n; i++){
+                    if(nums[i]!= val){
+                        nums[j] = nums[i];
+                            j++;
+                    }
+                   
+            }
+            return j;
+        
+    }
+1. };``
